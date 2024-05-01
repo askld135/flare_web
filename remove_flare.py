06b7424c34fd1124f3270036477eed5c6c4a8afc
askld135 <w3218822@naver.com>
2024-05-01 14:45:25 +0900

    resize

--- remove_flare.py
+++ remove_flare.py
@@ -29,7 +29,7 @@ def remove_flare(model, image):
 
 
     if min(h, w) >= 2048:
-        inputs = T.functional.center_crop(inputs, [2048,2048])  # resize
+        inputs = T.functional.resize(inputs, [2048,2048])  # resize
         inputs_low = F.interpolate(inputs, (512,512), mode='area')
         pred_scene_low = model(inputs_low).clamp(0.0, 1.0)
         pred_flare_low = synthesis.remove_flare(inputs_low, pred_scene_low)
